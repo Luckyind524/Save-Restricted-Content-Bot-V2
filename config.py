c0d9848f7a26f39c33eee0b4e209dcab2079c254
Luckyind524 <156711897+Luckyind524@users.noreply.github.com>
2025-05-08 15:54:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,13 +13,13 @@ YTUB_COOKIES = """
 # write here yt cookies
 """
 
-API_ID = int(getenv("API_ID", "23237831"))
-API_HASH = getenv("API_HASH", "017dc661ca1432ac2fe5ecb62499d88f")
+API_ID = int(getenv("API_ID", "23939637"))
+API_HASH = getenv("API_HASH", "477f51720ede3eef6997dbc442151c43")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "6103594386").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "6425525488").split()))
 MONGO_DB = getenv("MONGO_DB", "")
 LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002421745690"))
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002557248555"))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "50"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
 WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
